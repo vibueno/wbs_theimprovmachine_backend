df0f4bc50b1c7516034bc7dc4a0368c2296a82b9
vibueno <20514260+vibueno@users.noreply.github.com>
2021-03-04 10:11:14 +0100

    Add tests for messagetemplate.templateHasParams

--- src/utils/__tests__/messagetemplate.test.ts
+++ src/utils/__tests__/messagetemplate.test.ts
@@ -1,7 +1,21 @@
-import { fillInMsgTemplate } from '../messagetemplate';
+import { templateHasParams, fillInMsgTemplate } from '../messagetemplate';
 import { TemplateInvalidError, TemplateParamsError } from '../error';
 
 describe('Module message template', () => {
+  describe('Function templateHasParams', () => {
+    test('it should return true if the template contains any param', () => {
+      expect(templateHasParams('Hello ${param1}')).toBe(true);
+    });
+
+    test('it should return false if the params are not well defined', () => {
+      expect(templateHasParams('Hello ${param1')).toBe(false);
+    });
+
+    test('it should return false if the string has no params', () => {
+      expect(templateHasParams('Hello Earth')).toBe(false);
+    });
+  });
+
   describe('Function fillInMsgTemplate', () => {
     test('it should throw an error if the template contains no params', () => {
       expect(() =>
