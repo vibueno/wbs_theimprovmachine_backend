ec912abb608816e4b4f226d088e24316ea5d0f5b
vibueno <20514260+vibueno@users.noreply.github.com>
2021-04-08 11:58:38 +0200

    Add cors config

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({ origin: true }));
 
 // Routes
 import appRoutes from './routes/App';
